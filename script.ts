3f43f7e5957dc1976c114977783204879f4b9c1b
trabur <travis.burandt@gmal.com>
2020-12-16 13:26:24 -0600

    demonstration

--- script.ts
+++ script.ts
@@ -1,6 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+var jwt = require('jsonwebtoken')
 var w3cwebsocket = require("websocket").w3cwebsocket
 var Socket = require("phoenix").Socket
 var socket = new Socket("wss://printedbasics.gigalixirapp.com/socket", {transport: w3cwebsocket})
@@ -8,18 +10,60 @@ socket.connect()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
-
+  // channel
   let channel = socket.channel("MAIN", {token: "abc"})
   channel.join()
-    .receive("ok", ({ messages }: any) => console.log("joined MAIN channel", messages) )
-    .receive("error", ({ reason }: any) => console.log("failed to join MAIN channel", reason) )
+    .receive("ok", ({ messages }: any) => console.log("joined MAIN channel", messages))
+    .receive("error", ({ reason }: any) => console.log("failed to join MAIN channel", reason))
     .receive("timeout", () => console.log("still waiting..."))
 
-  channel.on("room:123", (msg: any) => console.log("received:", msg))
-  channel.push("room:broadcast", {room: '123', message: 'hello world'})
+
+  // functions
+  channel.on("room:register", function (data: any) {
+    console.log('sign:data:', data.message.sign)
+    let msg = jwt.sign(data.message.sign, '1337-secret-shhhhh', { expiresIn: '1h' })
+
+    channel.push("room:broadcast", {
+      room: data.message.output,
+      message: msg
+    })
+  })
+
+  channel.on("room:login", function (data: any) {
+    console.log('verify:token:', data.message.token)
+    let msg = jwt.verify(data.message.token, '1337-secret-shhhhh', { expiresIn: '1h' })
+
+    channel.push("room:broadcast", {
+      room: data.message.output,
+      message: msg
+    })
+  })
+
+  channel.on("room:users", async function (data: any) {
+    const allUsers = await prisma.user.findMany()
+
+    channel.push("room:broadcast", {
+      room: data.message.output,
+      message: allUsers
+    })
+  })
+
+  // demonstration
+  let outputRoom1 = uuidv4()
+  let outputRoom2 = uuidv4()
+  channel.on(`room:${outputRoom1}`, async function (message: any) {
+    console.log(message)
+  })
+  channel.on(`room:${outputRoom2}`, async function (message: any) {
+    console.log(message)
+  })
+  channel.push("room:broadcast", {
+    room: 'register',
+    message: {
+      sign: { userId: 1 },
+      output: outputRoom1
+    }
+  })
 }
 
 main()
