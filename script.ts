a1a2f5407118aec581cc900462902c3e7a475e7b
trabur <travis.burandt@gmal.com>
2020-12-15 22:10:10 -0600

    orm

--- script.ts
+++ script.ts
@@ -1,10 +1,25 @@
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
+
+var w3cwebsocket = require("websocket").w3cwebsocket
+var Socket = require("phoenix").Socket
+var socket = new Socket("wss://printedbasics.gigalixirapp.com/socket", {transport: w3cwebsocket})
+socket.connect()
 
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+
+  let channel = socket.channel("MAIN", {token: "abc"})
+  channel.join()
+    .receive("ok", ({ messages }: any) => console.log("joined MAIN channel", messages) )
+    .receive("error", ({ reason }: any) => console.log("failed to join MAIN channel", reason) )
+    .receive("timeout", () => console.log("still waiting..."))
+
+  channel.on("room:123", (msg: any) => console.log("received:", msg))
+  channel.push("room:broadcast", {room: '123', message: 'hello world'})
 }
 
 main()
