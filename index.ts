e5ce694582c674de841d6a60e1ed7a86d07a4c2f
trabur <travis.burandt@gmal.com>
2020-12-20 16:55:12 -0600

    index

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as users from './lib/users'
+import * as tenants from './lib/tenants'
+import * as platform from './lib/tenants/platform'
+import * as services from './lib/tenants/services'
+
+export default {
+  users,
+  tenants,
+  platform,
+  services
+}
