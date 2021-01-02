44c6ec67a220e57dd5f74e45e0417ffd5991a715
trabur <travis.burandt@gmal.com>
2021-01-02 12:31:11 -0600

    remove listeners

--- src/tenants.ts
+++ src/tenants.ts
@@ -1,6 +1,4 @@
 // libraries
-import { PrismaClient } from '../prisma/node_modules/.prisma/client'
-const prisma = new PrismaClient()
 import { v4 as uuidv4 } from 'uuid';
 
 // elixir socket
@@ -15,38 +13,6 @@ channel.join()
   .receive("ok", ({ messages }: any) => console.log("joined MAIN channel", messages))
   .receive("error", ({ reason }: any) => console.log("failed to join MAIN channel", reason))
   .receive("timeout", () => console.log("still waiting..."))
-
-// listener references
-let ref1: any
-let ref2: any
-let ref3: any
-let ref4: any
-
-
-// listener functions
-import onRegister from './users/onRegister'
-import onLogin from './users/onLogin'
-import onUsers from './users/onUsers'
-import onRemove from './users/onRemove'
-
-/******
- * trigger methods
- ******/
-function run () { 
-  // start listening
-  ref1 = channel.on("room:register", onRegister(prisma, channel))
-  ref2 = channel.on("room:login", onLogin(prisma, channel))
-  ref3 = channel.on("room:users", onUsers(prisma, channel))
-  ref4 = channel.on("room:remove", onRemove(prisma, channel))
-}
-
-function stop () { 
-  // quit listening
-  channel.off("room:register", ref1)
-  channel.off("room:login", ref2)
-  channel.off("room:users", ref3)
-  channel.off("room:remove", ref4)
-}
 
 /******
  * trigger actions
@@ -112,8 +78,6 @@ function remove (email: any, password: any, callback: any) {
  * trigger library
  ******/
 export {
-  stop,
-  run,
   all,
   register,
   login,
