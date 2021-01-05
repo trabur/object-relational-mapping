3c387be6de58299bc9d798681e9646710cc20c72
trabur <travis.burandt@gmal.com>
2021-01-04 21:49:08 -0600

    joined MAIN channel

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ export class ORM {
     // phoenix channel
     let channel = socket.channel("MAIN", {token: "abc"})
     channel.join()
-      .receive("ok", ({ messages }: any) => console.log("users: joined MAIN channel", messages))
-      .receive("error", ({ reason }: any) => console.log("users: failed to join MAIN channel", reason))
+      .receive("ok", ({ messages }: any) => console.log("joined MAIN channel", messages))
+      .receive("error", ({ reason }: any) => console.log("failed to join MAIN channel", reason))
       .receive("timeout", () => console.log("still waiting..."))
 
     // methods
