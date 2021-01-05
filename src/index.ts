552b3a9273f553308c61e59e41c1868df4f7f36f
trabur <travis.burandt@gmal.com>
2021-01-04 21:48:18 -0600

    orm constructor

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,21 @@ export class ORM {
     socket.connect()
     
     // phoenix channel
-    this.channel = socket.channel("MAIN", {token: "abc"})
-    this.channel.join()
+    let channel = socket.channel("MAIN", {token: "abc"})
+    channel.join()
       .receive("ok", ({ messages }: any) => console.log("users: joined MAIN channel", messages))
       .receive("error", ({ reason }: any) => console.log("users: failed to join MAIN channel", reason))
       .receive("timeout", () => console.log("still waiting..."))
 
     // methods
-    this.users = new Users(this.channel)
-    this.tenants = new Tenants(this.channel)
-    this.platforms.publishSubscribe = new PublishSubscribe(this.channel)
-    this.platforms.counter = new Counter(this.channel)
-    this.platforms.logging = new Logging(this.channel)
+    this.users = new Users(channel)
+    this.tenants = new Tenants(channel)
+    this.platforms.publishSubscribe = new PublishSubscribe(channel)
+    this.platforms.counter = new Counter(channel)
+    this.platforms.logging = new Logging(channel)
     
     return this
   }
-  channel = null
   users = null
   tenants = null
   platforms = {
