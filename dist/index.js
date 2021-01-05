dc972b69a5872c63fca176fe56fc3dfb0d21d904
trabur <travis.burandt@gmal.com>
2021-01-04 21:53:25 -0600

    build

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,6 @@ var counter_1 = require("./platforms/counter");
 var logging_1 = require("./platforms/logging");
 var ORM = /** @class */ (function () {
     function ORM(socket) {
-        this.channel = null;
         this.users = null;
         this.tenants = null;
         this.platforms = {
@@ -19,23 +18,23 @@ var ORM = /** @class */ (function () {
         // could be socket for node.js or web
         socket.connect();
         // phoenix channel
-        this.channel = socket.channel("MAIN", { token: "abc" });
-        this.channel.join()
+        var channel = socket.channel("MAIN", { token: "abc" });
+        channel.join()
             .receive("ok", function (_a) {
             var messages = _a.messages;
-            return console.log("users: joined MAIN channel", messages);
+            return console.log("joined MAIN channel", messages);
         })
             .receive("error", function (_a) {
             var reason = _a.reason;
-            return console.log("users: failed to join MAIN channel", reason);
+            return console.log("failed to join MAIN channel", reason);
         })
             .receive("timeout", function () { return console.log("still waiting..."); });
         // methods
-        this.users = new users_1.Users(this.channel);
-        this.tenants = new tenants_1.Tenants(this.channel);
-        this.platforms.publishSubscribe = new publishSubscribe_1.PublishSubscribe(this.channel);
-        this.platforms.counter = new counter_1.Counter(this.channel);
-        this.platforms.logging = new logging_1.Logging(this.channel);
+        this.users = new users_1.Users(channel);
+        this.tenants = new tenants_1.Tenants(channel);
+        this.platforms.publishSubscribe = new publishSubscribe_1.PublishSubscribe(channel);
+        this.platforms.counter = new counter_1.Counter(channel);
+        this.platforms.logging = new logging_1.Logging(channel);
         return this;
     }
     return ORM;
